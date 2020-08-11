68af36d6a34b49b446b4e6b0c13d730f27582f2c
Darius Mitchell <dariuskmitchell@outlook.com>
2020-08-10 21:24:06 -0400

    Almost done

--- myGuessingGame.py
+++ myGuessingGame.py
@@ -16,4 +16,22 @@ print("There is a number between 1 & 10. " + user_name + ", if you guess it righ
 # This keeps track of the guesses.
 guesscount = 0
 
+# This is the while loop. While the guess count is equal to or less than 4 you can keep trying.
+while guesscount <= 4:
+
+# This is converting the input to an integer and assigning it to the variable.   
+    guess = int(input())
+
+# This adds one each time the code is cycled.
+    guesscount += 1
+
+# This is the comparison operator.
+    if guess > random_number:
+        print("Try again, it's too high")
+    if guess < random_number:
+        print("Try again, it's too low!")
+    if guess == random_number:
+
+# This keyword ends the loop.
+        break
 
